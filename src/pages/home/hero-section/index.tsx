a8e3aeb6da66e60426f13bcbd318e6897179db6f
noyonalways <noyonrahman2003@gmail.com>
2024-07-31 23:41:10 +0600

    feat: Make the Navbar responsive

--- src/pages/home/hero-section/index.tsx
+++ src/pages/home/hero-section/index.tsx
@@ -2,8 +2,8 @@ interface IProps {}
 
 const HeroSection: React.FC<IProps> = () => {
   return (
-    <section className="pb-20">
-      <div className="container">This is Hero Section</div>
+    <section className="">
+      <div className="container"></div>
     </section>
   );
 };
