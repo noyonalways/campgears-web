6d99ab9923d7cf26b5cc8844b518b693bf5c243d
noyonalways <noyonrahman2003@gmail.com>
2024-08-04 00:07:14 +0600

    feat: api base url added on environment variable

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: import.meta.env.VITE_BASE_URL,
 });
 
 export const baseApi = createApi({
-  reducerPath: "basiApi",
+  reducerPath: "baseApi",
   baseQuery: baseQuery,
   endpoints: () => ({}),
   tagTypes: ["product"],
