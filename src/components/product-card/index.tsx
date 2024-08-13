f6759fc717bcd693ec72f1ae3d27c1c7f050191b
noyonalways <noyonrahman2003@gmail.com>
2024-08-13 19:11:32 +0600

    feat: product card title modified

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -32,9 +32,9 @@ const ProductCard: React.FC<IProps> = ({
       : 0;
 
   return (
-    <div className="font-montserrat p-5 py-5 text-left transform duration-500 hover:-translate-y-2 hover:shadow-2xl cursor-pointer rounded border  border-slate-200 lg:border-slate-100/70 group overflow-hidden">
+    <div className="font-montserrat p-5 py-5 text-left transform duration-500 hover:-translate-y-2 hover:shadow-xl cursor-pointer rounded border  border-slate-200 lg:border-slate-100/70 group overflow-hidden">
       <img
-        className="lg:size-52 object-cover mx-auto group-hover:scale-105 duration-200 rounded"
+        className="size-64 lg:size-52 :object-cover mx-auto group-hover:scale-105 duration-200 rounded"
         src={image}
         alt=""
       />
@@ -44,7 +44,7 @@ const ProductCard: React.FC<IProps> = ({
       >
         {category}
       </Link>
-      <h1 className="text-2xl mb-3 h-20 lg:mb-5 lg:h-28 font-medium">
+      <h1 className="text-xl mb-3 h-26 lg:mb-5 lg:h-20 font-medium">
         {name.length > 40 ? name.substring(0, 40) + "..." : name}
       </h1>
       <div className="space-x-1 flex mb-5">
