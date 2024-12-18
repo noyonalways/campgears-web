4e4e550b1691d253a115f5e195af4b0aeb04f45f
noyonalways <noyonrahman2003@gmail.com>
2024-12-19 01:20:55 +0600

    feat: products page product card added

--- src/app/(main-layout)/shop/products/page.tsx
+++ src/app/(main-layout)/shop/products/page.tsx
@@ -1,13 +1,17 @@
 import { PageBreadcrumb } from "@/components/breadcrumbs";
+import ProductCard from "@/components/cards/product-card";
+import { Button } from "@/components/ui/button";
+import SortByDropdown from "@/modules/products/sortby-dropdown";
+import { LayoutGrid, List } from "lucide-react";
 
 const ProductsPage = () => {
   return (
     <section>
       <PageBreadcrumb currentPage="Products" />
       <div className="container lg:my-10">
-        <div className="flex h-screen">
+        <div className="flex">
           {/* left sidebar */}
-          <div className="basis-[22%] border-r border-dashed">
+          <div className="hidden lg:block basis-[22%] border-r border-dashed">
             <div className="lg:pr-4">
               <div className="flex justify-between items-center">
                 <h3 className="font-medium">Filters</h3>
@@ -21,10 +25,42 @@ const ProductsPage = () => {
           {/* main contents */}
           <div className="flex-1">
             <div className="lg:pl-5">
-              <div className="bg-primary/10 flex justify-end lg:p-6 rounded">
-                <div className="bg-primary text-white w-full lg:text-4xl font-bold lg:p-6 rounded max-w-md">
+              <div className="bg-primary/10 flex justify-end p-4 lg:p-6 rounded">
+                <div className="bg-primary text-white w-full text-xl lg:text-4xl font-bold p-4 lg:p-6 rounded max-w-md">
                   Explore the amazing gears and Accessories
                 </div>
+              </div>
+
+              <div className="flex justify-between items-center my-4">
+                <div className="flex items-center space-x-2">
+                  <span className="text-sm md:text-base">Sort By</span>
+                  {/* <Button variant={`secondary`}>
+                    <span>Popularity</span>
+                    <ChevronDown />
+                  </Button> */}
+                  <SortByDropdown />
+                </div>
+                <div className="flex space-x-2 items-center">
+                  <span className="text-muted-foreground text-sm">View as</span>
+                  <div className="flex space-x-2">
+                    <Button size={`icon`}>
+                      <LayoutGrid size={20} />
+                    </Button>
+                    <Button size={`icon`} variant={`secondary`}>
+                      <List size={20} />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 lg:gap-6">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
               </div>
             </div>
           </div>
---
+++ src/components/cards/product-card/index.tsx
@@ -0,0 +1,83 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Eye, Heart, Star } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface IProps {
+  item?: string;
+}
+
+const ProductCard = ({}: IProps) => {
+  return (
+    <div className="bg-secondary rounded group">
+      <div className="flex justify-center mt-4 mb-2 relative ">
+        <Image src={`/tent.png`} width={150} height={150} alt="product-image" />
+
+        <div className="flex items-center justify-center absolute bg-background opacity-0 -bottom-10 group-hover:-bottom-2 group-hover:opacity-100 text-muted-foreground rounded duration-200">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="hover:text-primary px-10 py-1">
+                  <Eye size={20} />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Quick View</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="hover:text-primary px-10 py-1">
+                  <Heart size={20} />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Add to Wishlist</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+
+      <div className="p-4">
+        <p className="text-sm text-muted-foreground mb-2">Category</p>
+        <Link
+          href={`/shop/products/slug`}
+          className="font-medium mb-2 inline-block"
+        >
+          Product Full Title Here
+        </Link>
+        <div className="flex space-x-1 text-orange-400 mb-2">
+          <Star size={14} />
+          <Star size={14} />
+          <Star size={14} />
+          <Star size={14} />
+          <Star size={14} />
+        </div>
+        <p className="text-sm text-muted-foreground mb-1 flex items-center space-x-1">
+          <span>Qty:</span>
+          <span>16</span>
+        </p>
+        <div className="flex items-end space-x-1 mb-2">
+          <h3 className="text-primary font-medium">$35.00</h3>
+          <span className="line-through text-muted-foreground text-sm">
+            $45.00
+          </span>
+        </div>
+
+        <button className="bg-background px-4 py-2 flex items-center justify-center w-full rounded-full hover:bg-primary/10 hover:text-primary font-medium text-sm md:text-base">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
