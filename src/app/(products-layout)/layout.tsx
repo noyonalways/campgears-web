94cb02677d8b63621742ce6bb6d062beed58fe12
noyonalways <noyonrahman2003@gmail.com>
2024-12-22 18:45:39 +0600

    products layout and product details page modified

--- src/app/(products-layout)/layout.tsx
+++ src/app/(products-layout)/layout.tsx
@@ -1,77 +1,75 @@
 import { PageBreadcrumb } from "@/components/breadcrumbs";
-import ProductCard from "@/components/cards/product-card";
 import { Button } from "@/components/ui/button";
 import ProductsMobileSideControls from "@/modules/products/products-mobile-side-controls";
 import ProductsSideControls from "@/modules/products/products-side-controls";
 import SortByDropdown from "@/modules/products/sortby-dropdown";
-import { getProducts } from "@/services/product";
 import { Footer, Navbar } from "@/shared";
-import { IProduct } from "@/types/product";
 import { LayoutGrid, List } from "lucide-react";
-const ProductsPage = async () => {
-  const res = await getProducts();
-  const products = res?.data as IProduct[];
+import { ReactNode } from "react";
 
+interface IProps {
+  children: ReactNode;
+}
+
+const ProductsLayout = async ({ children }: IProps) => {
   return (
     <>
       <Navbar />
-      <section>
-        <PageBreadcrumb currentPage="Products" />
-        <div className="container lg:my-10 mb-10">
-          <div className="flex items-start">
-            {/* left sidebar */}
-            <div className="hidden lg:block basis-[22%] border-r border-dashed">
-              <ProductsSideControls />
-            </div>
+      <main>
+        <section>
+          <PageBreadcrumb currentPage="Products" />
+          <div className="container lg:my-10 mb-10">
+            <div className="flex items-start">
+              {/* left sidebar */}
+              <div className="hidden lg:block basis-[22%] border-r border-dashed">
+                <ProductsSideControls />
+              </div>
 
-            {/* main contents */}
-            <div className="flex-1">
-              <div className="lg:pl-5">
-                <div className="bg-primary/10 flex justify-end p-4 lg:p-6 rounded">
-                  <div className="bg-primary text-white w-full text-xl lg:text-4xl font-bold p-4 lg:p-6 rounded max-w-md">
-                    Explore the amazing gears and Accessories
-                  </div>
-                </div>
-
-                {/* products mobile side controls */}
-                <div className="mt-4">
-                  <ProductsMobileSideControls />
-                </div>
-
-                <div className="flex justify-between items-center my-4">
-                  <div className="flex items-center space-x-2">
-                    <span className="text-sm md:text-base">Sort By</span>
-                    <SortByDropdown />
-                  </div>
-                  <div className="flex space-x-2 items-center">
-                    <span className="text-muted-foreground text-sm">
-                      View as
-                    </span>
-                    <div className="flex space-x-2">
-                      <Button size={`icon`}>
-                        <LayoutGrid size={20} />
-                      </Button>
-                      <Button size={`icon`} variant={`secondary`}>
-                        <List size={20} />
-                      </Button>
+              {/* main contents */}
+              <div className="flex-1">
+                <div className="lg:pl-5">
+                  <div className="bg-primary/10 flex justify-end p-4 lg:p-6 rounded">
+                    <div className="bg-primary text-white w-full text-xl lg:text-4xl font-bold p-4 lg:p-6 rounded max-w-md">
+                      Explore the amazing gears and Accessories
                     </div>
                   </div>
-                </div>
 
-                {/* products cards */}
-                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 lg:gap-6">
-                  {products.map((product) => (
-                    <ProductCard key={product._id} />
-                  ))}
+                  {/* products mobile side controls */}
+                  <div className="mt-4">
+                    <ProductsMobileSideControls />
+                  </div>
+
+                  <div className="flex justify-between items-center my-4">
+                    <div className="flex items-center space-x-2">
+                      <span className="text-sm md:text-base">Sort By</span>
+                      <SortByDropdown />
+                    </div>
+                    <div className="flex space-x-2 items-center">
+                      <span className="text-muted-foreground text-sm">
+                        View as
+                      </span>
+                      <div className="flex space-x-2">
+                        <Button size={`icon`}>
+                          <LayoutGrid size={20} />
+                        </Button>
+                        <Button size={`icon`} variant={`secondary`}>
+                          <List size={20} />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* products cards */}
+                  {children}
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
       <Footer />
     </>
   );
 };
 
-export default ProductsPage;
+export default ProductsLayout;
---
+++ src/app/(products-layout)/shop/products/page.tsx
@@ -0,0 +1,20 @@
+import ProductCard from "@/components/cards/product-card";
+import { getProducts } from "@/services/product";
+import { IProduct } from "@/types/product";
+const ProductsPage = async () => {
+  const res = await getProducts();
+  const products = res?.data as IProduct[];
+
+  return (
+    <>
+      {/* products cards */}
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 lg:gap-6">
+        {products.map((product) => (
+          <ProductCard key={product._id} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductsPage;
